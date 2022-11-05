e49c7ac784c0ace4bc0531d3adcf85335128a0fc
Augustin <adebureaux@gmail.com>
2022-11-05 19:49:04 +0100

    Finished kata00 and kata01

---
+++ ex05/kata01.py
@@ -0,0 +1,9 @@
+# Put this at the top of your kata01.py file
+kata = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+
+for s in kata:
+	print(s, kata[s], sep=" was created by ")
