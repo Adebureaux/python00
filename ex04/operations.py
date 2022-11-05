b6d070610b3f9e47d80b3c3eb46ee26aafce757e
Augustin <adebureaux@gmail.com>
2022-11-05 19:27:54 +0100

    finished ex04

--- ex04/operations.py
+++ ex04/operations.py
@@ -21,10 +21,12 @@ else:
 		if not isdigit(arg):
 			sys.exit("Error: arguments must be integers")
 	numbers = [int(arg) for arg in sys.argv[1:]]
-	print(f"Sum: {sum(numbers)}")
-	print(f"Difference: {numbers[0] - numbers[1]}")
-# Sum: A+B
-# Difference: A-B
-# Product: A*B
-# Quotient: A/B
-# Remainder: A%B
+	print(f"Sum:		{sum(numbers)}")
+	print(f"Difference:	{numbers[0] - numbers[1]}")
+	print(f"Product:	{numbers[0] * numbers[1]}")
+	if numbers[1] is not 0:
+		print(f"Quotient:	{numbers[0] / numbers[1]}")
+		print(f"Remainder:	{numbers[0] % numbers[1]}")
+	else:
+		print(f"Quotient:	ERROR (division by zero)")
+		print(f"Remainder:	ERROR (modulo by zero)")
