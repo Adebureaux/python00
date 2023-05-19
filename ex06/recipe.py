1582f21082fe82ebe3c959fbceb6ca7330e22bd1
Augustin Debureaux <adebureaux@gmail.com>
2023-05-19 19:55:07 +0200

    ex06 finished

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -4,9 +4,9 @@ import sys
 
 cookbook = {}
 
-cookbook['Sandwich'] = { 'ingredients': [ 'ham', 'bread', 'cheese', 'tomatoes'], 'meal': 'lunch', 'prep_time': 10 }
-cookbook['Cake'] = { 'ingredients': ['flour', 'sugar', 'eggs'], 'meal': 'dessert', 'prep_time': 60 }
-cookbook['Salad'] = { 'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'], 'meal': 'lunch', 'prep_time': 15 }
+cookbook['sandwich'] = { 'ingredients': [ 'ham', 'bread', 'cheese', 'tomatoes'], 'meal': 'lunch', 'prep_time': 10 }
+cookbook['cake'] = { 'ingredients': ['flour', 'sugar', 'eggs'], 'meal': 'dessert', 'prep_time': 60 }
+cookbook['salad'] = { 'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'], 'meal': 'lunch', 'prep_time': 15 }
 
 
 # Part Two
@@ -21,19 +21,30 @@ def print_names():
 		print('There is no recipe in the cookbook')
 
 def print_details(name):
-	if cookbook.get(name):
-		print('Recipe details of ' + name + ' :')
-		print(cookbook.get(name))
+	recipe = cookbook.get(name)
+	if recipe:
+		print("Recipe details of '" + name + "' :")
+		if recipe['ingredients']:
+			print(f"Ingredient(s) list: {recipe['ingredients']}")
+		if recipe['meal']:
+			print(f"To be eaten for {recipe['meal']}")
+		print(f"Takes {recipe['prep_time']} minutes of cooking")
 	else:
 		print("No details available for the recipe '" + name + "'")
 
 def delete_recipe(name):
 	if cookbook.get(name):
 		del cookbook[name]
+		print(f"'{name}' successfully removed")
+	else:
+		print(f"'{name}' does not exist")
 
 def add_recipe():
-	name = input('>>> Enter a name:\n')
-	print('>>> Enter ingredients:')
+	while True:
+		name = input('>> Enter a name:\n')
+		if name:
+				break
+	print('>> Enter ingredients:')
 	ingredients = []
 	while True:
 		ingredient = input()
@@ -41,10 +52,10 @@ def add_recipe():
 				ingredients.append(ingredient)
 		else:
 				break
-	meal = input('>>> Enter meal type:\n')
+	meal = input('>> Enter meal type:\n')
 	while True:
 		try:
-			prep_time = int(input('>>> Enter a preparation time:\n'))
+			prep_time = int(input('>> Enter a preparation time:\n'))
 			if prep_time >= 0:
 				break
 			else:
@@ -61,7 +72,7 @@ def print_usage():
 		print('List of available option:')
 		print('\t1: Add a recipe')
 		print('\t2: Delete a recipe')
-		print('\t3: Print the recipe')
+		print('\t3: Print a recipe')
 		print('\t4: Print the cookbook')
 		print('\t5: Quit\n')
 
@@ -71,13 +82,15 @@ def _add():
 	print('Recipe successfully added !')
 
 def _delete():
-	delete_recipe()
+	name = input('Please type a recipe name\n>> ')
+	delete_recipe(name)
 
 def _print_recipe():
-	print_names()
+	name = input('Please type a recipe name\n>> ')
+	print_details(name)
 
 def _print_cookbook():
-	print('print cookbook')
+	print_names()
 
 def _quit():
 	print('Cookbook closed. Goodbye !')
@@ -87,7 +100,7 @@ if __name__ == '__main__':
 	print_usage()
 	fct = [ _add, _delete, _print_recipe, _print_cookbook, _quit ]
 	while True:
-		option = input('\nPlease select an option\n>> ')
+		option = input('\nPlease select an option\n>>> ')
 		if not option.isdigit() or option < '1' or option > '5':
 			print_usage()
 			continue
