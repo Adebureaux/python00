cb86ef503792d1395f95d89f56a5d90990854470
Augustin Debureaux <adebureaux@gmail.com>
2023-05-21 16:38:12 +0200

    Finished

--- ex10/loading.py
+++ ex10/loading.py
@@ -1,28 +1,27 @@
-import time
+from time import sleep, time
 
 def ft_progress(lst):
-		total = len(lst)
-		bar_length = 30
-
-		for i, item in enumerate(lst):
-				progress = (i + 1) / total
-				completed = int(progress * bar_length)
-				remaining = bar_length - completed
-				bar = "[" + "#" * completed + "-" * remaining + "]"
-				percentage = int(progress * 100)
-
-				print(f"Progress: {bar} {percentage}%", end="\r")
-
-				# Process the current item
-
-				yield
-
-		print()
+	total = len(lst)
+	bar_length = 20
+	start_time = time()
+	max_num_len = len(str(total))
+	for i, item in enumerate(lst, 1):
+		progress = i / total
+		completed = int(progress * bar_length)
+		remaining = bar_length - completed
+		bar = "[" + "=" * completed + ">" + "-" * remaining + "]"
+		percentage = int(progress * 100)
+		elapsed_time = time() - start_time
+		eta = (elapsed_time / progress) - elapsed_time if progress > 0 else 0
+		progress_str = f"{i:>{max_num_len}}/{total}"
+		print(f"ETA: {eta:.2f}s [{percentage:3}%]{bar} {progress_str} | elapsed time {elapsed_time:.2f}s", end="\r")
+		yield item
+	return total
 
 listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
-	ret += 1
-	time.sleep(0.01)
-print()
-print(ret)
+	ret += (elem + 3) % 5
+	sleep(0.01)
+	# print()
+	# print(ret)
